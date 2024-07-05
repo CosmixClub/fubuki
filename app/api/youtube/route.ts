e4b065c7450c6a552502d7bcac0b2dc91676bcde
João Pedro <nmjoaopedro22@gmail.com>
2024-07-05 12:27:41 -0300

    feat: implementa API de informações de vídeos do YT

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,11 +1,32 @@
 import { NextResponse } from "next/server";
+import ytdl from "ytdl-core";
+import { z } from "zod";
 
 import { api, handlerConfig } from "@/lib/api";
 
 const router = api();
 
+const getInputs = z.object({
+	url: z.string().url(),
+});
+
 router.get(async (req, ctx) => {
-	return NextResponse.json({ message: "Hello, World!" });
+	const params = req.nextUrl.searchParams;
+	const inputs = getInputs.parse(Object.fromEntries(params));
+	const url = new URL(inputs.url);
+
+	const domains = ["www.youtube.com", "youtube.com", "m.youtube.com", "youtu.be"];
+	if (!domains.includes(url.hostname)) {
+		throw new Error("Invalid youtube domain.");
+	}
+
+	const info = await ytdl.getInfo(url.toString(), { lang: "pt" });
+	const data = {
+		streaming: info.player_response.streamingData,
+		details: info.player_response.videoDetails,
+	};
+
+	return NextResponse.json({ message: "Sucesso", data });
 });
 
 const handler = router.handler(handlerConfig);
--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,22 @@
 import { type NextHandler, createEdgeRouter } from "next-connect";
 import { RequestContext } from "next/dist/server/base-server";
 import { NextRequest, NextResponse } from "next/server";
+import { ZodError } from "zod";
 
 export type Middleware = (req: NextRequest, ctx: RequestContext, next: NextHandler) => Promise<void>;
 
 export const handlerConfig = {
 	onError: (err: any, req: NextRequest, _ctx: RequestContext) => {
+		if (err instanceof ZodError) {
+			return NextResponse.json(
+				{
+					message: "Houve um erro de validação dos dados enviados.",
+					issues: err.issues,
+				},
+				{ status: 400 },
+			);
+		}
+
 		console.error(`❌ API failed: ${err.message || "Unknown error"}`, { err });
 
 		// if (err instanceof UnknownError) {
