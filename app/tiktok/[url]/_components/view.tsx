af7cf156beaba0070cd6d1ca6879db9fefedbe97
João Pedro <nmjoaopedro22@gmail.com>
2024-07-06 16:09:45 -0300

    feat: implementa página do tiktok

--- app/tiktok/[url]/_components/view.tsx
+++ app/tiktok/[url]/_components/view.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { memo, useCallback } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import { GetOutputs } from "@/app/api/tiktok/route";
-import { ActionIcon, AspectRatio, Button, RangeSlider, Select, TextInput } from "@mantine/core";
+import { ActionIcon, AspectRatio, Button, TextInput } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 
 export type ViewProps = {
@@ -18,16 +17,9 @@ type Fields = {
 	url: string;
 };
 
-// const getThumbnail = (data: GetOutputs["data"]) => {
-// 	// @ts-expect-error - This is a valid operation
-// 	return data.details.thumbnail.thumbnails.toReversed()[0];
-// };
-
 export const View: React.FC<ViewProps> = memo(function View({ url, data }) {
 	const {
 		handleSubmit,
-		setValue,
-		watch,
 		register,
 		formState: { isSubmitting },
 	} = useForm<Fields>({
@@ -43,30 +35,19 @@ export const View: React.FC<ViewProps> = memo(function View({ url, data }) {
 		[router],
 	);
 
-	// const download: SubmitHandler<Fields> = useCallback(
-	// 	async fields => {
-	// 		const response = await fetch("/api/youtube", {
-	// 			method: "POST",
-	// 			headers: {
-	// 				"Content-Type": "application/json",
-	// 			},
-	// 			body: JSON.stringify({ url, quality: fields.quality }),
-	// 		});
+	const download: SubmitHandler<Fields> = useCallback(async () => {
+		const response = await fetch(data.video);
 
-	// 		const type = response.headers.get("Content-Type")?.split(";")[0].split("/")[1] || "mp4";
+		const blob = await response.blob();
+		const media = URL.createObjectURL(blob);
 
-	// 		const blob = await response.blob();
-	// 		const media = URL.createObjectURL(blob);
+		const a = document.createElement("a");
+		a.href = media;
+		a.download = data.desc + "." + "mp4";
+		a.click();
 
-	// 		const a = document.createElement("a");
-	// 		a.href = media;
-	// 		a.download = data.details.title + "." + type;
-	// 		a.click();
-
-	// 		URL.revokeObjectURL(media);
-	// 	},
-	// 	[data.details.title, url],
-	// );
+		URL.revokeObjectURL(media);
+	}, [data.desc, data.video]);
 
 	return (
 		<section className="flex h-full w-full flex-col gap-6 px-10">
@@ -94,13 +75,22 @@ export const View: React.FC<ViewProps> = memo(function View({ url, data }) {
 				<div className="flex w-full flex-col gap-4 md:max-w-[75%]">
 					<div className="relative">
 						<AspectRatio ratio={16 / 9}>
-							{/* <Image
-								alt="Thumbnail do vídeo"
-								className="h-full w-full rounded-lg object-cover"
-								src={getThumbnail(data).url}
-								width={getThumbnail(data).width}
-								height={getThumbnail(data).height}
-							/> */}
+							<video
+								width={720}
+								height={1280}
+								controls
+								preload="none"
+								muted
+								autoPlay
+							>
+								<source
+									src={data.video}
+									type="video/mp4"
+								/>
+								<span className="px-4 text-center text-xl font-medium">
+									Pré-visualização de mídia do TikTok não suportada.
+								</span>
+							</video>
 						</AspectRatio>
 					</div>
 					<div className="flex flex-col">
@@ -111,7 +101,7 @@ export const View: React.FC<ViewProps> = memo(function View({ url, data }) {
 				<div className="flex h-full w-full flex-col gap-2 rounded-lg border border-neutral-300 bg-neutral-100/50 p-2 backdrop-blur-sm md:max-w-[25%]">
 					<Button
 						className="w-full"
-						// onClick={handleSubmit(download)}
+						onClick={handleSubmit(download)}
 						loading={isSubmitting}
 					>
 						Baixar
